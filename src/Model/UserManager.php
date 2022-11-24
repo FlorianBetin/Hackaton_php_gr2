cee083a4256b8df5397a4fbcb5cd29f304fb42cf
MaxenceCodeFR <maxence.hattabi@gmail.com>
2022-11-24 17:46:16 +0100

    début de soirée

---
+++ src/Model/UserManager.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Model;
+
+class RegisterManager extends AbstractManager
+{
+    public const TABLE = 'user';
+
+    /**
+     * Insert new item in database
+     */
+    public function insert(array $user): string
+    {
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE .
+            " (lastname, firstname, birthdate, email, password)
+        VALUES (:lastname, :firstname, :birthdate, :email, :password)");
+        $statement->bindValue(':lastname', $user['lastname'], \PDO::PARAM_STR);
+        $statement->bindValue(':firstname', $user['firstname'], \PDO::PARAM_STR);
+        $statement->bindValue(':email', $user['email'], \PDO::PARAM_STR);
+        $statement->execute();
+        return $this->pdo->lastInsertId();
+    }
+
+    public function selectByOneByEmail(string $email): array|false
+    {
+        $query = "SELECT * FROM " . self::TABLE . " WHERE email=:email";
+        $statement = $this->pdo->prepare($query);
+        $statement->bindValue('email', $email);
+        $statement->execute();
+
+        return $statement->fetch();
+    }
+}
